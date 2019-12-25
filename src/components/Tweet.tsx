49730fb4512e6a553ac50331a162d96977506207
Florian Aumeier (FG-230) <Florian.AB.Aumeier@bmw.de>
2019-12-25 17:08:15 +0100

    step 12: tweet page

---
+++ src/components/Tweet.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {Avatar, Card, Skeleton} from 'antd'
+import {connect} from "react-redux"
+import {TweetsState} from "../reducers/tweets"
+import {User} from "../actions/users"
+import {Tweet} from "../actions/tweets"
+import 'antd/dist/antd.css'
+
+function TweetDetails({authedUser, users, tweets, match}: any) {
+  const {Meta} = Card
+  const author: User = users[authedUser]
+  const tweet: Tweet = tweets[match.params.id]
+  return <Card>
+    <Skeleton loading={!tweet || !author} avatar active>
+      {tweet && author && (
+        <Meta
+          avatar={<Avatar src={author.avatarURL}/>}
+          title={author.name}
+          description={tweet.text}
+        />
+      )}
+    </Skeleton>
+  </Card>
+}
+
+
+const mapStateToProps = (state: { authedUser: string, users: any, tweets: TweetsState }) => ({
+  authedUser: state.authedUser,
+  users: state.users,
+  tweets: state.tweets
+})
+
+
+export default connect(mapStateToProps)(TweetDetails)
